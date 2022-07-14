7ff1effee3020c50a11c2d2b044471e9e16c1311
binudhir <binu.dhir@gmail.com>
2022-07-14 14:51:11 +0530

    Pattern.java file Updated

--- JAVA/Pattern.java
+++ JAVA/Pattern.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pattern {
     public static void main(String[] args) {
-       int n=26, z=0;
+        System.out.println("Enter a Number:");
+        Scanner sc=new Scanner(System.in);        
+       int n=sc.nextInt();
+       int z=0;
        for(int i=1; i<=n; i++){
            for(int j=n-1;j>=i; j--){
                System.out.print(" ");
@@ -10,6 +15,7 @@ public class Pattern {
            }
            z+=2;
            System.out.println();
-       } 
+       }
+       sc.close(); 
     }
 }
