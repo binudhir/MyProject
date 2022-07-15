7e194127eb18dc8c9cd5c6c4e4ecbdf9e02e3fbd
binudhir <binu.dhir@gmail.com>
2022-07-15 11:24:21 +0530

    Update the LinkedListExample Java file

--- JAVA/LinkedListExample.java
+++ JAVA/LinkedListExample.java
@@ -14,5 +14,23 @@ public class LinkedListExample{
         System.out.println("Initial Linkedlist" + li);
         li.set(5, "Programmer.");
         System.out.println("Updated Linkedlist" + li);
+        li.remove(5);
+        System.out.println(li);
+        li.remove("is");
+        li.remove(3);
+        System.out.println(li);
+        li.add("is");
+        li.add("a");
+        li.add("good");
+        li.add("Person");
+        System.out.println(li);
+        for(int i=0; i<li.size(); i++){
+            System.out.print(li.get(i) + " ");
+        }
+        System.out.println();
+        for(String str : li){
+            System.out.print(str + " ");
+        }
+        System.out.println();
     }
 }
